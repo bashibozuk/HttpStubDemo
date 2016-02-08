7d5b51c98078b0b33b2fd8e54342c6a71c51f634
Vasil Atanasov <vsatanasov@gmail.com>
2016-02-08 19:39:28 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vasil
+ * Date: 28.01.16
+ * Time: 18:42
+ */
+ini_set('display_errors', 'On');
+ini_set('error_reporting', E_ALL | E_STRICT);
+
+require_once 'vendor/autoload.php';
+
+\HttpStub\Settings::set(\HttpStub\Settings::PATH, realpath(__DIR__ . '/.data'));
+\HttpStub\Server\Request\RequestHandler::init([
+    'request_type' => \HttpStub\Server\Request\Request::TYPE_HTTP
+]);
+\HttpStub\Server\Request\RequestHandler::handleRequest();
