0bfc1a7cbe526ef3dc565d388da2478dee4e580c
Vasil Atanasov <vsatanasov@gmail.com>
2016-02-08 21:57:09 +0200

    README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+###Demo setup for [HttpStub](https://github.com/bashibozuk/HttpStub)
+###Installation:
+    - clone the repo
+    - download and setup composer if you are not currently using it
+    - run `composer install` to install the HttpStub package
--- index.php
+++ index.php
@@ -12,6 +12,6 @@ require_once 'vendor/autoload.php';
 
 \HttpStub\Settings::set(\HttpStub\Settings::PATH, realpath(__DIR__ . '/.data'));
 \HttpStub\Server\Request\RequestHandler::init([
-    'request_type' => \HttpStub\Server\Request\Request::TYPE_HTTP
+    'request_type' => \HttpStub\Server\Request\Request::TYPE_REST
 ]);
 \HttpStub\Server\Request\RequestHandler::handleRequest();
